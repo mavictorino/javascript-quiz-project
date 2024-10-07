d86be92bcd52f8a64c92e35d4ff0f9744f5b88c8
Mariana Victorino <victorino.marii@gmail.com>
2024-10-07 15:39:27 +0100

    Day One - Mariana Victorino e Mariana Frazão

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,37 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
     
-    // 3. moveToNextQuestion()
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    };
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    };
+    moveToNextQuestion() {
+        return this.currentQuestionIndex ++;
+    };
+    shuffleQuestions() {
+        for (let i = this.questions.length -1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            this.questions[i] = this.questions[j];
+        };
+        
+    };
+    checkAnswer(answer) {
+        this.answer = answer;
+        return this.correctAnswers ++;
+    };
 
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    };
 }
